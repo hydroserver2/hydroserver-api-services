4a0ff118892e4eae2afcfc3f125e9eaacbb4cf28
daniel-slaugh <daniel.slaugh@hotmail.com>
2023-03-01 16:52:20 -0700

    Add creation views for all models. Add first datastream setup page

--- sites/static/js/modal.js
+++ sites/static/js/modal.js
@@ -1,44 +1,26 @@
-let modal = document.getElementById("add-sensor-modal");
-document.getElementById("add-sensor-button").onclick = function()  { modal.style.display = "block";};
-document.getElementsByClassName("close")[0].onclick = function() { modal.style.display = "none"; };
-window.onclick = function(event) { if (event.target === modal) modal.style.display = "none";};
+let currentModal;
+const modalButtons = document.querySelectorAll('.modal-button');
 
-// Only show manufacturer and model if we're dealing with an instrumentDeployment
-let sensorDropdown = document.getElementById("id_method_type");
-let manufacturerField = document.getElementById("manufacturer-field");
-let modelField = document.getElementById("model-field");
-manufacturerField.style.display = "none";
-modelField.style.display = "none";
-sensorDropdown.onchange = function() {
-  if (sensorDropdown.value === "instrumentDeployment") {
-    manufacturerField.style.display = "block";
-    modelField.style.display = "block";
-  } else {
-    manufacturerField.style.display = "none";
-    modelField.style.display = "none";
+//Open modal onclick
+modalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modalId = button.dataset.modal;
+    currentModal = document.getElementById(modalId);
+    currentModal.style.display = 'block';
+  });
+});
+
+// Close modal onclick
+const closeButtons = document.querySelectorAll('.close');
+closeButtons.forEach(closeButton => {
+  closeButton.addEventListener('click', () => {
+    currentModal.style.display = 'none';
+  });
+});
+
+// Close modal onclick anywhere outside of modal
+window.addEventListener('click', event => {
+  if (event.target === currentModal) {
+    currentModal.style.display = 'none';
   }
-};
-
-// Add new method to drop down list
-let submit_btn = document.getElementById("submit-sensor-data");
-let form = document.getElementById("add-sensor-form");
-form.addEventListener("submit", function(event) {
-    event.preventDefault();
-    let csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value;
-    fetch(submit_btn.dataset.url, {
-        method: "POST",
-        headers: {"X-CSRFToken": csrftoken,},
-        body: new FormData(form),
-    })
-    .then(response => {
-        if (response.ok)
-            return response.json();
-        else
-            throw new Error("Network response was not ok");
-    })
-    .then(data => {
-        modal.style.display = "none";
-        window.location.reload();
-    })
-    .catch(error => { console.error("Error adding sensor:", error);});
 });
