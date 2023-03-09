cb6877f0fe883b57865fed7f27318731331608ee
Ken Lippold <klippold@Kens-MacBook-Pro-2.local>
2023-03-09 16:31:15 -0700

    Static files test

--- hydroserver/settings.py
+++ hydroserver/settings.py
@@ -168,8 +168,7 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_ROOT = 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
