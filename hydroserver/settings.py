840a9709294603187229b5968a0f7eb6ddaf0249
Ken Lippold <klippold@Kens-MacBook-Pro-2.local>
2023-03-09 17:05:59 -0700

    Updating staticfiles settings.

--- hydroserver/settings.py
+++ hydroserver/settings.py
@@ -168,7 +168,7 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
