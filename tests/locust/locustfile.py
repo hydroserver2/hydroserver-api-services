a59df7208fc7cbc009230cae3924a893259c61a1
Ken Lippold <klippold@Kens-MacBook-Pro-2.local>
2023-03-27 11:51:15 -0600

    Added initial Locust testing file.

---
+++ tests/locust/locustfile.py
@@ -0,0 +1,8 @@
+from locust import HttpUser, task
+
+
+class GetThingCollection(HttpUser):
+
+    @task
+    def get_thing_collection(self):
+        self.client.get('/sensorthings/v1.1/Things', auth=('kenneth.lippold@usu.edu', 'tHw90Js@8d'))
