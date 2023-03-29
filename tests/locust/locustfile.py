3d7c129cee4449561904564f698b9ec99f3bee6d
Ken Lippold <klippold@Kens-MacBook-Pro-2.local>
2023-03-29 15:40:07 -0600

    Fixed observation auth issue.

--- tests/locust/locustfile.py
+++ tests/locust/locustfile.py
@@ -1,12 +1,43 @@
 import os
+import json
+import random
 from locust import HttpUser, task
+from datetime import datetime, timedelta
 
 
 class GetThingCollection(HttpUser):
 
-    @task
+    timestamp = datetime(2023, 4, 1, 0, 0, 0)
+
+    # @task
     def get_thing_collection(self):
         self.client.get(
             '/sensorthings/v1.1/Things',
             auth=(os.getenv('LOCUST_TEST_USERNAME'), os.getenv('LOCUST_TEST_PASSWORD'))
         )
+
+    @task
+    def post_observations(self):
+
+        data_array = []
+
+        for _ in range(96):
+            data_array.append([
+                self.timestamp.strftime('%Y-%m-%d %H:%M:%S'),
+                random.randint(0, 100)
+            ])
+
+            self.timestamp = self.timestamp + timedelta(minutes=15)
+
+        request_body = [
+            {
+                'Datastream': {
+                    '@iot.id': '376be82c-b3a1-4d96-821b-c7954b931f94'
+                },
+                'components': [
+                    'resultTime',
+                    'result'
+                ],
+                'dataArray': data_array
+            }
+        ]
