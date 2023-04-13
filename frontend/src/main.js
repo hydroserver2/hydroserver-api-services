c4c214467050128a592a290ae971595d25d3ad2d
daniel-slaugh <daniel.slaugh@hotmail.com>
2023-04-13 10:28:19 -0600

    Add Material Design Icons

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue';
 import router from './router';
 import { store } from './store';
 
+import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
