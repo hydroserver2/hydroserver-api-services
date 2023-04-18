0e79869121b821c0fbd9236a17ad03e02e48a82a
Ken Lippold <klippold@Kens-MacBook-Pro-2.local>
2023-04-18 13:58:47 -0600

    Route frontend requests with Vite.

--- hydroserver/urls.py
+++ hydroserver/urls.py
@@ -1,16 +1,16 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 from hydroserver.api import api
 from hydroserver.views import index
 
 urlpatterns = [
-    path('', index),
     path('admin/', admin.site.urls),
     path('sensorthings/', include('sensorthings.urls')),
-    path('api/', api.urls)
+    path('api/', api.urls),
+    re_path('.*', index),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
