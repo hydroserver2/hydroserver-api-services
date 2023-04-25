7427893063b676c45b324ee532fbb5cb71686964
daniel-slaugh <daniel.slaugh@hotmail.com>
2023-04-25 13:15:05 -0600

    Add Footer

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,23 +1,30 @@
 <script>
-import Navbar from "./components/Navbar.vue";
+import Navbar from "@/components/Navbar.vue";
+import Footer from "@/components/Footer.vue";
 export default {
   name: 'App',
   components: {
+    Footer,
     Navbar
   }
 }
 </script>
 
 <template>
-  <v-app>
-  <header>
+  <v-app class="main-layout">
     <Navbar/>
-  </header>
-  <v-main>
-    <router-view/>
-  </v-main>
+    <v-main class="main-content"><router-view/></v-main>
+    <Footer/>
   </v-app>
 </template>
 
-<style scoped>
+<style>
+.main-layout {
+  display: flex;
+  flex-direction: column;
+}
+
+.main-content {
+  min-height: 100vh;
+}
 </style>
