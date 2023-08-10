350835c63f2ea217f12c7a970c83d08bb63b9589
Ken Lippold <klippold@Kens-MBP-2.lan>
2023-08-10 12:09:01 -0700

    Updates to OAuth

--- core/auth/oauth.py
+++ core/auth/oauth.py
@@ -1,3 +1,4 @@
+from ninja import Router
 from authlib.integrations.django_client import OAuth
 from hydroserver import settings
 
@@ -10,8 +11,49 @@ oauth.register(
     client_kwargs={'scope': 'openid email profile'}
 )
 
+orcid_router = Router(tags=['ORCID'])
+
+
+@orcid_router.get('/login')
+def orcid_login(request):
+    redirect_uri = request.build_absolute_uri('/auth/orcid/auth')
+
+    if 'X-Forwarded-Proto' in request.headers:
+        redirect_uri = redirect_uri.replace('http:', request.headers['X-Forwarded-Proto'] + ':')
+
+    return oauth.orcid.authorize_redirect(request, redirect_uri)
+
+
+@orcid_router.get('/auth')
+def orcid_auth(request):
+    token = oauth.orcid.authorize_access_token(request)
+
+    print(token)
+
+
 oauth.register(
     name='google',
     server_metadata_url=settings.AUTHLIB_OAUTH_CLIENTS['google']['server_metadata_url'],
     client_kwargs={'scope': 'openid email profile'}
 )
+
+google_router = Router(tags=['Google'])
+
+
+@google_router.get('/login')
+def google_login(request):
+    redirect_uri = request.build_absolute_uri('/auth/google/auth')
+
+    if 'X-Forwarded-Proto' in request.headers:
+        redirect_uri = redirect_uri.replace('http:', request.headers['X-Forwarded-Proto'] + ':')
+
+    return oauth.google.authorize_redirect(request, redirect_uri)
+
+
+@google_router.get('/auth')
+def google_auth(request):
+
+    token = oauth.google.authorize_access_token(request)
+    # request.session['user'] = token['userinfo']
+
+    return token
