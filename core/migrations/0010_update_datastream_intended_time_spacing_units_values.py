011e56b7d4387ca85bb1cd5b9e7a38609c4a11f9
daniel-slaugh <daniel.slaugh@hotmail.com>
2024-01-04 15:53:32 -0700

    Handle valid intended_time_spacing_units in migrations

--- core/migrations/0010_update_datastream_intended_time_spacing_units_values.py
+++ core/migrations/0010_update_datastream_intended_time_spacing_units_values.py
@@ -1,5 +1,5 @@
 from django.db import migrations
-
+import uuid
 
 def convert_time_spacing_uuid_to_string(apps, schema_editor):
     units = apps.get_model('core', 'Unit')
@@ -9,6 +9,13 @@ def convert_time_spacing_uuid_to_string(apps, schema_editor):
     datastreams = apps.get_model('core', 'Datastream')
     for datastream in datastreams.objects.all():
         if datastream.intended_time_spacing_units is not None:
+            # To handle the case where the intended_time_spacing_units are already converted to strings,
+            # continue if the intended_time_spacing_units isn't a UUID
+            try:
+                uuid.UUID(datastream.intended_time_spacing_units)
+            except ValueError:
+                continue
+            
             try:
                 time_spacing_unit_string = next((
                     time_spacing_unit.name.lower() + 's' for time_spacing_unit in time_spacing_units
