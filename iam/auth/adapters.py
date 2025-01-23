1eb27057348343f7a3e2e0c5ac8fd26c0f33a9da
Ken Lippold <klippold@Kens-MacBook-Pro-2.local>
2025-01-23 11:51:09 -0800

    Fixed account adapter.

--- iam/auth/adapters.py
+++ iam/auth/adapters.py
@@ -12,6 +12,24 @@ class AccountAdapter(DefaultAccountAdapter):
     def is_open_for_signup(self, request: HttpRequest):
         return settings.ACCOUNT_SIGNUP_ENABLED
 
+    def save_user(self, request, user, form, commit=True):
+        user = super().save_user(request, user, form, commit=False)
+        data = form.cleaned_data
+
+        profile = ProfilePatchBody(
+            **{
+                "middle_name": data.get("middle_name") or None,
+                "phone": data.get("phone") or None,
+                "address": data.get("address") or None,
+                "link": data.get("link") or None,
+                "user_type": data.get("user_type") or None,
+                "organization": data.get("organization") or None,
+            }
+        )
+        profile.save(user)
+
+        return user
+
 
 class HeadlessAdapter(DefaultHeadlessAdapter):
     def serialize_user(self, user) -> Dict[str, Any]:
@@ -24,31 +42,3 @@ class HeadlessAdapter(DefaultHeadlessAdapter):
         user_response["profile"] = user_profile.dict(by_alias=True)
 
         return user_response
-
-
-class SocialAccountAdapter(DefaultSocialAccountAdapter):
-    def save_user(self, request, sociallogin, form=None):
-        user = sociallogin.user
-        user.set_unusable_password()
-
-        if form:
-            data = form.cleaned_data
-            user = get_account_adapter().save_user(request, user, form, commit=False)
-            profile = ProfilePatchBody(
-                **{
-                    "middle_name": data.get("middle_name") or None,
-                    "phone": data.get("phone") or None,
-                    "address": data.get("address") or None,
-                    "link": data.get("link") or None,
-                    "user_type": data.get("user_type") or None,
-                    "organization": data.get("organization") or None,
-                }
-            )
-            profile.save(user)
-
-        else:
-            get_account_adapter().populate_username(request, user)
-
-        sociallogin.save(request)
-
-        return user
