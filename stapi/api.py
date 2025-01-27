ed412909509b84f2839af9fc70ad578411ad02cc
Ken Lippold <klippold@Kens-MBP-2.lan>
2025-01-27 13:23:31 -0800

    Resolved merge conflicts.

--- stapi/api.py
+++ stapi/api.py
@@ -1,19 +1,19 @@
 from sensorthings import SensorThingsExtension
 from sensorthings.factories import SensorThingsEndpointHookFactory
 from stapi import schemas
-from hydroserver.auth import JWTAuth, BasicAuth, anonymous_auth
+from hydroserver.security import session_auth, basic_auth, anonymous_auth
 
 
 hydroserver_extension = SensorThingsExtension(
     endpoint_hooks=[
         SensorThingsEndpointHookFactory(
             endpoint_name='list_datastreams',
-            view_authentication=[JWTAuth(), BasicAuth(), anonymous_auth],
+            view_authentication=[session_auth, basic_auth, anonymous_auth],
             view_response_schema=schemas.DatastreamListResponse
         ),
         SensorThingsEndpointHookFactory(
             endpoint_name='get_datastream',
-            view_authentication=[JWTAuth(), BasicAuth(), anonymous_auth],
+            view_authentication=[session_auth, basic_auth, anonymous_auth],
             view_response_schema=schemas.DatastreamGetResponse
         ),
         SensorThingsEndpointHookFactory(
@@ -33,11 +33,11 @@ hydroserver_extension = SensorThingsExtension(
         ),
         SensorThingsEndpointHookFactory(
             endpoint_name='list_features_of_interest',
-            view_authentication=[JWTAuth(), BasicAuth(), anonymous_auth],
+            view_authentication=[session_auth, basic_auth, anonymous_auth],
         ),
         SensorThingsEndpointHookFactory(
             endpoint_name='get_feature_of_interest',
-            view_authentication=[JWTAuth(), BasicAuth(), anonymous_auth],
+            view_authentication=[session_auth, basic_auth, anonymous_auth],
         ),
         SensorThingsEndpointHookFactory(
             endpoint_name='create_feature_of_interest',
@@ -56,11 +56,11 @@ hydroserver_extension = SensorThingsExtension(
         ),
         SensorThingsEndpointHookFactory(
             endpoint_name='list_historical_locations',
-            view_authentication=[JWTAuth(), BasicAuth(), anonymous_auth],
+            view_authentication=[session_auth, basic_auth, anonymous_auth],
         ),
         SensorThingsEndpointHookFactory(
             endpoint_name='get_historical_location',
-            view_authentication=[JWTAuth(), BasicAuth(), anonymous_auth],
+            view_authentication=[session_auth, basic_auth, anonymous_auth],
         ),
         SensorThingsEndpointHookFactory(
             endpoint_name='create_historical_location',
@@ -79,12 +79,12 @@ hydroserver_extension = SensorThingsExtension(
         ),
         SensorThingsEndpointHookFactory(
             endpoint_name='list_locations',
-            view_authentication=[JWTAuth(), BasicAuth(), anonymous_auth],
+            view_authentication=[session_auth, basic_auth, anonymous_auth],
             view_response_schema=schemas.LocationListResponse
         ),
         SensorThingsEndpointHookFactory(
             endpoint_name='get_location',
-            view_authentication=[JWTAuth(), BasicAuth(), anonymous_auth],
+            view_authentication=[session_auth, basic_auth, anonymous_auth],
             view_response_schema=schemas.LocationGetResponse
         ),
         SensorThingsEndpointHookFactory(
@@ -104,22 +104,22 @@ hydroserver_extension = SensorThingsExtension(
         ),
         SensorThingsEndpointHookFactory(
             endpoint_name='list_observations',
-            view_authentication=[JWTAuth(), BasicAuth(), anonymous_auth],
+            view_authentication=[session_auth, basic_auth, anonymous_auth],
             view_response_schema=schemas.ObservationListResponse
         ),
         SensorThingsEndpointHookFactory(
             endpoint_name='get_observation',
-            view_authentication=[JWTAuth(), BasicAuth(), anonymous_auth],
+            view_authentication=[session_auth, basic_auth, anonymous_auth],
             view_response_schema=schemas.ObservationGetResponse
         ),
         SensorThingsEndpointHookFactory(
             endpoint_name='create_observation',
-            view_authentication=[JWTAuth(), BasicAuth(), anonymous_auth],
+            view_authentication=[session_auth, basic_auth, anonymous_auth],
             view_body_schema=schemas.ObservationPostBody
         ),
         SensorThingsEndpointHookFactory(
             endpoint_name='create_observations',
-            view_authentication=[JWTAuth(), BasicAuth(), anonymous_auth],
+            view_authentication=[session_auth, basic_auth, anonymous_auth],
             view_body_schema=schemas.ObservationDataArrayPostBody
         ),
         SensorThingsEndpointHookFactory(
@@ -129,20 +129,20 @@ hydroserver_extension = SensorThingsExtension(
         ),
         SensorThingsEndpointHookFactory(
             endpoint_name='delete_observation',
-            view_authentication=[JWTAuth(), BasicAuth(), anonymous_auth],
+            view_authentication=[session_auth, basic_auth, anonymous_auth],
         ),
         SensorThingsEndpointHookFactory(
             endpoint_name='delete_observations',
-            view_authentication=[JWTAuth(), BasicAuth(), anonymous_auth],
+            view_authentication=[session_auth, basic_auth, anonymous_auth],
         ),
         SensorThingsEndpointHookFactory(
             endpoint_name='list_observed_properties',
-            view_authentication=[JWTAuth(), BasicAuth(), anonymous_auth],
+            view_authentication=[session_auth, basic_auth, anonymous_auth],
             view_response_schema=schemas.ObservedPropertyListResponse
         ),
         SensorThingsEndpointHookFactory(
             endpoint_name='get_observed_property',
-            view_authentication=[JWTAuth(), BasicAuth(), anonymous_auth],
+            view_authentication=[session_auth, basic_auth, anonymous_auth],
             view_response_schema=schemas.ObservedPropertyGetResponse
         ),
         SensorThingsEndpointHookFactory(
@@ -162,12 +162,12 @@ hydroserver_extension = SensorThingsExtension(
         ),
         SensorThingsEndpointHookFactory(
             endpoint_name='list_sensors',
-            view_authentication=[JWTAuth(), BasicAuth(), anonymous_auth],
+            view_authentication=[session_auth, basic_auth, anonymous_auth],
             view_response_schema=schemas.SensorListResponse
         ),
         SensorThingsEndpointHookFactory(
             endpoint_name='get_sensor',
-            view_authentication=[JWTAuth(), BasicAuth(), anonymous_auth],
+            view_authentication=[session_auth, basic_auth, anonymous_auth],
             view_response_schema=schemas.SensorGetResponse
         ),
         SensorThingsEndpointHookFactory(
@@ -187,12 +187,12 @@ hydroserver_extension = SensorThingsExtension(
         ),
         SensorThingsEndpointHookFactory(
             endpoint_name='list_things',
-            view_authentication=[JWTAuth(), BasicAuth(), anonymous_auth],
+            view_authentication=[session_auth, basic_auth, anonymous_auth],
             view_response_schema=schemas.ThingListResponse
         ),
         SensorThingsEndpointHookFactory(
             endpoint_name='get_thing',
-            view_authentication=[JWTAuth(), BasicAuth(), anonymous_auth],
+            view_authentication=[session_auth, basic_auth, anonymous_auth],
             view_response_schema=schemas.ThingGetResponse
         ),
         SensorThingsEndpointHookFactory(
