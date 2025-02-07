3663e16d4c18ae6036c45e2f178a7fc6b6e4b21a
Ken Lippold <klippold@Kens-MacBook-Pro-2.local>
2025-02-07 09:17:43 -0800

    Updated requirements.txt

--- hydroserver/settings.py
+++ hydroserver/settings.py
@@ -109,7 +109,7 @@ WSGI_APPLICATION = "hydroserver.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-os.environ["DATABASE_URL"] = config("DATABASE_URL", default=f"sqlite:///db.sqlite3")
+os.environ["DATABASE_URL"] = config("DATABASE_URL", default=f"postgresql://hsdbadmin:admin@localhost:5432/hydroserver")
 
 DATABASES = {
     "default": dj_database_url.config(
