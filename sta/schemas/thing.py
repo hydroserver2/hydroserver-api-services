be1721740b5826dacbbf50f4836012c12e4c0633
Ken Lippold <klippold@Kens-MacBook-Pro-2.local>
2025-02-10 13:37:30 -0800

    Fixed Thing location aliases

--- sta/schemas/thing.py
+++ sta/schemas/thing.py
@@ -1,7 +1,7 @@
 import uuid
 from typing import Optional
 from ninja import Schema, Field
-from pydantic import field_validator
+from pydantic import field_validator, AliasChoices
 from country_list import countries_for_language
 from hydroserver.schemas import BaseGetResponse, BasePostBody, BasePatchBody
 from .tag import TagGetResponse
@@ -11,14 +11,20 @@ valid_country_codes = [code for code, _ in countries_for_language('en')]
 
 
 class LocationFields(Schema):
-    latitude: float = Field(..., ge=-90, le=90, validation_alias="location.latitude")
-    longitude: float = Field(..., ge=-180, le=180, validation_alias="location.longitude")
+    latitude: float = Field(..., ge=-90, le=90,
+                            validation_alias=AliasChoices("latitude", "location.latitude"))
+    longitude: float = Field(..., ge=-180, le=180,
+                             validation_alias=AliasChoices("longitude", "location.longitude"))
     elevation_m: Optional[float] = Field(None, ge=-99999, le=99999, alias="elevation_m",
-                                         validation_alias="location.elevation_m")
-    elevation_datum: Optional[str] = Field(None, max_length=255, validation_alias="location.elevation_datum")
-    state: Optional[str] = Field(None, max_length=200, validation_alias="location.state")
-    county: Optional[str] = Field(None, max_length=200, validation_alias="location.county")
-    country: Optional[str] = Field(None, max_length=2, validation_alias="location.country")
+                                         validation_alias=AliasChoices("elevation_m", "location.elevation_m"))
+    elevation_datum: Optional[str] = Field(None, max_length=255,
+                                           validation_alias=AliasChoices("elevationDatum", "location.elevation_datum"))
+    state: Optional[str] = Field(None, max_length=200,
+                                 validation_alias=AliasChoices("state", "location.state"))
+    county: Optional[str] = Field(None, max_length=200,
+                                  validation_alias=AliasChoices("county", "location.county"))
+    country: Optional[str] = Field(None, max_length=2,
+                                   validation_alias=AliasChoices("country", "location.country"))
 
     @field_validator('country', mode='after')
     def check_country_code(cls, value):
